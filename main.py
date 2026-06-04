56c1362b7876e43d5d47775b5c30301d2af39223
Hamid <hamid.mohammadian.ce@gmail.com>
2026-06-04 16:21:16 +0330

    Implement full period tracker system with add/edit/delete, statistics, prediction, and JSON persistence

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ class PeriodTracker:
         if not date:
             return False, "Date cannot be empty."
 
-        if date  in self.period_dates:
+        if date in self.period_dates:
             return False, "This date has already been recorded."
 
         if self.period_dates and date <= self.period_dates[-1]:
@@ -74,6 +74,28 @@ class PeriodTracker:
         removed_date = self.period_dates.pop(index-1)
         return True, f"{removed_date} deleted successfully."
 
+    def edit_period_date(self, index, new_date):
+        if not self.period_dates:
+            return False, "No dates to edit."
+
+        if new_date in self.period_dates:
+            return False, "This date has already been recorded."
+
+        if index < 1 or index > len(self.period_dates):
+            return False, "Invalid index."
+
+        prev_date = self.period_dates[index-2] if index > 1 else None
+        next_date = self.period_dates[index] if index < len(self.period_dates) else None
+
+        if prev_date and new_date <= prev_date:
+            return False, "New date must be after previous date."
+
+        if next_date and new_date >= next_date:
+            return False, "New date must be before next date."
+
+        self.period_dates[index-1] = new_date
+        return True, "date edited successfully."
+
     def save_data(self):
         dates = self.to_dict()
 
@@ -113,7 +135,8 @@ def display_menu():
     print("3. Show cycle statistics")
     print("4. Predict next period")
     print("5. Delete period date")
-    print("6. Save and Exit")
+    print("6. Edit period date")
+    print("7. Save and Exit")
 
 def receive_date(prompt):
     while True:
@@ -205,6 +228,19 @@ def main():
                 print('Invalid input.')
 
         elif choice == '6':
+            print(tracker)
+            try:
+                index = int(input('Enter number to edit: ').strip())
+                new_date = receive_date('Enter new period date (YYYY-MM-DD): ')
+
+                status, message = tracker.edit_period_date(index, new_date)
+
+                print(message)
+
+            except ValueError:
+                print('Invalid input.')
+
+        elif choice == '7':
             tracker.save_data()
             print('Good bye.')
             break
